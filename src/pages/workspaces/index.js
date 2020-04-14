c286948f52eb493806946f1f5ca3e2a02da1fb24
Vincent Herlemont <vincentherl@leszeros.com>
2020-04-14 09:30:12 +0000

    list workspaces

--- src/pages/workspaces/index.js
+++ src/pages/workspaces/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Nav from '../../components/nav'
+import {WorkspaceContext} from '../../context/WorkSpace'
+import {Link} from '@reach/router'
 
 let Workspaces = () => {
+    let {workspaces} = useContext(WorkspaceContext)
     return (
         <div>
             <div>List workspace</div>
+            <ul>
+                {workspaces.map(el => {
+                    return (
+                        <li key={el.id}>
+                            <Link to={'/workspaces/' + el.id}>
+                                ({el.id}) | {el.name}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
             <Nav links={[{to: '/workspaces/add', title: 'add'}]} />
         </div>
     )
